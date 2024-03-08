418540b35524f5022d2d9d28eb987d898f59b449
Luke McMeans <elukemcmeans@gmail.com>
2024-03-08 11:17:59 -0800

    Importing files to GitHub (created previously)

---
+++ content.js
@@ -0,0 +1,13 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.selectFirstResult) {
+      setTimeout(function () {
+        var firstResultLink = document.querySelector(".g h3 a");
+        if (firstResultLink) {
+          firstResultLink.click();
+        }
+      }, 2000);
+    }
+  });
+  
+  chrome.runtime.sendMessage({ selectFirstResult: true });
+  
